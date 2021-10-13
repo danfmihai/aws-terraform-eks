a24a595524f43b9caa9e2afe303467393feb1b29
mihai <misu3108@yahoo.com>
2021-10-13 20:51:52 +0000

    definition of vpc

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,14 @@
 resource "aws_vpc" "eks" {
   cidr_block       = "192.168.0.0/16"
   instance_tenancy = "default"
-
+  # Required by EKS. VPC must have DNS hostname and DNS 
+  # resolution support, or your nodes can't register with your cluster. 
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  enable_classiclink = false
+  enable_classiclink_dns_support = false
+  assign_generated_ipv6_cidr_block = false
+  
   tags = {
     Name = "eks"
   }
