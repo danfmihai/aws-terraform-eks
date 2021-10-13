3889e02bb31afa5dc709d54dc9f4d42514d19f18
mihai <misu3108@yahoo.com>
2021-10-13 20:55:28 +0000

    edited

--- vpc.tf
+++ vpc.tf
@@ -3,21 +3,21 @@ resource "aws_vpc" "eks" {
   instance_tenancy = "default"
   # Required by EKS. VPC must have DNS hostname and DNS 
   # resolution support, or your nodes can't register with your cluster. 
-  enable_dns_support = true
-  enable_dns_hostnames = true
-  enable_classiclink = false
-  enable_classiclink_dns_support = false
+  enable_dns_support               = true
+  enable_dns_hostnames             = true
+  enable_classiclink               = false
+  enable_classiclink_dns_support   = false
   assign_generated_ipv6_cidr_block = false
-  
+
   tags = {
     Name = "eks"
   }
 }
 
 output "vpc_id" {
-  value = aws_vpc.eks.id
+  value       = aws_vpc.eks.id
   description = "VPC id."
-  
+
   # Setting an output value as sensitive prevents Terraform from showing its value in plan and apply
   sensitive = false
 }
